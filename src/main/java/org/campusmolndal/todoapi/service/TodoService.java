175b6dcf6ffa1380c2ae3c19a07dc224911aef6b
PontusIK <pontus.kavrestad@gmail.com>
2024-10-11 23:13:52 +0200

    tomma service metoder

--- src/main/java/org/campusmolndal/todoapi/service/TodoService.java
+++ src/main/java/org/campusmolndal/todoapi/service/TodoService.java
@@ -1,5 +1,6 @@
 package org.campusmolndal.todoapi.service;
 
+import org.campusmolndal.todoapi.model.Todo;
 import org.campusmolndal.todoapi.repository.TodoRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +10,29 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TodoService {
     private final TodoRepository todoRepo;
+
+    public Object addTodo(Todo todo) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addTodo'");
+    }
+
+    public Object findTodoById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findTodoById'");
+    }
+
+    public Object findAllTodos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAllTodos'");
+    }
+
+    public Object updateTodo(Long id, Todo todo) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateTodo'");
+    }
+
+    public void deleteTodo(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteTodo'");
+    }
 }

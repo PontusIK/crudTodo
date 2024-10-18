ea2ec90ea4fcd811a39e3f4b4034bfc3c27d4472
PontusIK <01ponifv@gafe.molndal.se>
2024-10-18 14:56:45 +0200

    sonarlint rekommenderade ändringar

--- src/test/java/org/campusmolndal/todoapi/ServiceTest.java
+++ src/test/java/org/campusmolndal/todoapi/ServiceTest.java
@@ -1,7 +1,6 @@
 package org.campusmolndal.todoapi;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
@@ -23,7 +22,7 @@ import org.campusmolndal.todoapi.service.TodoService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ServiceTest {
+class ServiceTest {
     TodoRepository todoRepo;
     TodoService todoService;
 

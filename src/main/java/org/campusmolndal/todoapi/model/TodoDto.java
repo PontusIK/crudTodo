1efc4902ffceeebc0e58fec3d71eb6b9969f1907
PontusIK <01ponifv@gafe.molndal.se>
2024-10-20 22:16:06 +0200

    swagger annotaions todoDTO

--- src/main/java/org/campusmolndal/todoapi/model/TodoDto.java
+++ src/main/java/org/campusmolndal/todoapi/model/TodoDto.java
@@ -2,6 +2,7 @@ package org.campusmolndal.todoapi.model;
 
 import java.time.LocalDate;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TodoDto {
+    @Schema(description = "Name for todo object", example = "dishes")
     private String title;
+
+    @Schema(description = "Description for what to do", example = "do the dishes")
     private String description;
+
+    @Schema(description = "Date by which task is to be completed", example = "2024-11-11")
     private LocalDate deadLine;
 }
